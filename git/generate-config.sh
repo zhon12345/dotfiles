a505d991c8e3af90084babb856873f473b7dccd9
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2025-02-20 14:11:30 +0800

    🔨 Make sure Git respects XDG Base Directories

--- git/generate-config.sh
+++ git/generate-config.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+. ~/.profile
+
+if [[ ! -d "${XDG_CONFIG_HOME}/git" ]]; then
+	mkdir -p "${XDG_CONFIG_HOME}/git"
+fi
+
 declare -A git_config=(
 	["user.name"]="zhon12345"
 	["user.email"]="43035773+zhon12345@users.noreply.github.com"
 	["core.autocrlf"]="input"
 	["init.defaultBranch"]="main"
 )
+
+export GIT_CONFIG_GLOBAL="${XDG_CONFIG_HOME}/git/config"
 
 for key in "${!git_config[@]}"; do
 	git config --global "${key}" "${git_config[${key}]}"
