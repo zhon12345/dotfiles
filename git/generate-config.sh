af7b604c0ac047feee51ba60d2bea46bd336b813
zhon12345 <43035773+zhon12345@users.noreply.github.com>
2025-08-17 21:51:21 +0800

    🔧 Add ssh commit signing & config reset

--- git/generate-config.sh
+++ git/generate-config.sh
@@ -7,25 +7,37 @@ if [[ ! -d "${XDG_CONFIG_HOME}/git" ]]; then
 fi
 
 declare -A git_config=(
+	["commit.gpgsign"]="true"
+	["core.autocrlf"]="input"
+	["status.showStash"]="true"
+	["init.defaultBranch"]="main"
+	["gpg.format"]="ssh"
 	["user.name"]="zhon12345"
 	["user.email"]="43035773+zhon12345@users.noreply.github.com"
-	["core.autocrlf"]="input"
-	["init.defaultBranch"]="main"
-	["status.showStash"]="true"
+	["user.signingkey"]="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIApbUnuwCYVKebGrGJbexsjWOOc4XFRdQLqUZwUz9a8Q"
 	["url."https://github.com/".insteadOf"]="gh:"
 	["url."https://github.com/zhon12345/".insteadOf"]="zhon:"
 )
 
 export GIT_CONFIG_GLOBAL="${XDG_CONFIG_HOME}/git/config"
 
+if [[ -f "${GIT_CONFIG_GLOBAL}" ]]; then
+	rm -f "${GIT_CONFIG_GLOBAL}"
+fi
+
 for key in "${!git_config[@]}"; do
 	git config --global "${key}" "${git_config[${key}]}"
 done
+
+if [[ "$MACHINE" = "Windows" ]]; then
+	git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
+	git config --global gpg.ssh.program "C:/Windows/System32/OpenSSH/ssh-keygen.exe"
+fi
 
 if command -v code > /dev/null 2>&1; then
 	git config --global core.editor "code --wait"
 fi
 
 if [[ "$(uname -a)" =~ "WSL" ]]; then
-	git config --global credential.helper "/mnt/c/Program\\ Files/Git/mingw64/bin/git-credential-manager.exe"
+	git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
 fi
